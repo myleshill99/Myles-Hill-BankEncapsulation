f4e0c99170c6131b7ac9338c13392c6211f30e24
Myles Hill <mylehill99@gmail.com>
2023-11-28 22:18:17 -0500

    finished

---
+++ BankEncapsulation/BankAccount2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+
+    public class BankAccount2
+    {
+        private double  _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+            Console.WriteLine($"Stimulating a deposit of {amount} to your account!");
+
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace BankEncapsulation
+﻿
+
+using BankEncapsulation;
+using System.IO.Pipes;
+using System.Security.Cryptography.X509Certificates;
+
+var account = new BankAccount2();
+Console.WriteLine("How much would you like to deposit");
+var amountToDeposit = double.Parse(Console.ReadLine());
+
+varmylesAccount(amountToDeposit);
+
+Console.WriteLine($"Thank you! Your balance is now mylesAccount.GetBalnce()");
+
+void varmylesAccount(double amountToDeposit)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    throw new NotImplementedException();
 }
+
